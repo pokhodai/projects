da2a91a65138808517c802175d8d48e79be75b33
Sergei Pokhodai <sergei.pokhodai@gmail.com>
2023-06-02 08:03:36 +0400

    commit

--- app/src/main/java/ru/pokhodai/projects/data/remote/MarvelService.kt
+++ app/src/main/java/ru/pokhodai/projects/data/remote/MarvelService.kt
@@ -1,11 +1,16 @@
 package ru.pokhodai.projects.data.remote
 
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
+import ru.pokhodai.projects.model.response.marvel.CharacterResponse
 
 interface MarvelService {
 
     @GET("v1/public/characters")
-    suspend fun getCharacters() {
-
-    }
+    suspend fun getCharacters(
+        @Query("limit") limit: Int = 10,
+        @Query("offset") offset: Int = 0,
+        @Query("nameStartsWith") nameStartsWith: String? = null,
+    ): Response<CharacterResponse>
 }
---
+++ app/src/main/java/ru/pokhodai/projects/model/response/marvel/CharacterResponse.kt
@@ -0,0 +1,58 @@
+package ru.pokhodai.projects.model.response.marvel
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+class CharacterResponse(
+    @SerialName("data") val data: Data
+) {
+    @Serializable
+    class Data(
+        @SerialName("offset") val offset: Int = 0,
+        @SerialName("limit") val limit: Int = 0,
+        @SerialName("total") val total: Int = 0,
+        @SerialName("count") val count: Int = 0,
+        @SerialName("results") val results: List<Result> = emptyList()
+    ) {
+        @Serializable
+        class Result(
+          @SerialName("id") val id: Int = -1,
+          @SerialName("name") val name: String? = null,
+          @SerialName("description") val description: String? = null,
+          @SerialName("modified") val modified: String? = null,
+          @SerialName("thumbnail") val thumbnail: Thumbnail
+        ) {
+            @Serializable
+            class Thumbnail(
+                @SerialName("path") val path: String? = null,
+                @SerialName("extension") val extension: String? = null
+            )
+        }
+    }
+}
+
+//data class CharacterResponse(
+//    @SerializedName("data") val data: CharacterDataResponse
+//) {
+//    data class CharacterDataResponse(
+//        @SerializedName("offset") val offset: Int = 0,
+//        @SerializedName("limit") val limit: Int = 0,
+//        @SerializedName("total") val total: Int = 0,
+//        @SerializedName("count") val count: Int = 0,
+//        @SerializedName("results") val results: List<CharacterResultResponse> = emptyList()
+//    ) {
+//        data class CharacterResultResponse(
+//            @SerializedName("id") val id: Int = 0,
+//            @SerializedName("name") val name: String = "",
+//            @SerializedName("description") val description: String = "",
+//            @SerializedName("modified") val modified: String = "",
+//            @SerializedName("thumbnail") val thumbnail: CharacterThumbnailResponse
+//        ) {
+//            data class CharacterThumbnailResponse(
+//                @SerializedName("path") val path: String = "",
+//                @SerializedName("extension") val extension: String = ""
+//            )
+//        }
+//    }
+//}
